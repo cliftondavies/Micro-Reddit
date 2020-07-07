aca0d16167aab032ed56e01bc9e2c8b870855c8b
cliftondavies <clifton.davies@outlook.com>
2020-07-07 21:29:06 +0100

    Implement user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..16 }
+  validates :username, presence: true, length: { in: 4..15 }, uniqueness: true
 end
