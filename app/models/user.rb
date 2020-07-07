6bf55469dd761c08857358a2dc6855aa3938e09d
cliftondavies <clifton.davies@outlook.com>
2020-07-07 21:59:03 +0100

    Set up association between user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..16 }
   validates :username, presence: true, length: { in: 4..15 }, uniqueness: true
