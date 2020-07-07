64c3ac7f255ffd9958d1be48b2ba92153f5b3f6a
cliftondavies <clifton.davies@outlook.com>
2020-07-07 22:47:29 +0100

    Update post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, inverse_of: :post
   validates :url, presence: true
   validates_associated :user
   validates :user, presence: true
