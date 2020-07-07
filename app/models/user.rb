68799c1ac8c30001fb4e8d9eecae12bc8b120de1
cliftondavies <clifton.davies@outlook.com>
2020-07-07 22:00:28 +0100

    Update user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, inverse_of: :user
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..16 }
   validates :username, presence: true, length: { in: 4..15 }, uniqueness: true
